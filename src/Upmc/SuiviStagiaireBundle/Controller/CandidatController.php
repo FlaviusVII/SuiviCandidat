f1879442677a9da19229016a49035c4bf54c09ff
Licence <Licence@134.157.46.172>
2015-05-21 12:45:06 +0200

    Ajout modif et list candidat

--- src/Upmc/SuiviStagiaireBundle/Controller/CandidatController.php
+++ src/Upmc/SuiviStagiaireBundle/Controller/CandidatController.php
@@ -6,22 +6,53 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Upmc\SuiviStagiaireBundle\Entity\Candidat;
 use Upmc\SuiviStagiaireBundle\Form\CandidatType;
 
-class CandidatController extends Controller
-{
-    public function ajoutCandidatAction()
-    {
+class CandidatController extends Controller {
+
+    public function listCandidatAction() {
+         $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository("\Upmc\SuiviStagiaireBundle\Entity\Candidat");
+
+        $listCandidat = $repository->findAll();
+
+        return $this->render('UpmcSuiviStagiaireBundle:candidat:listCandidat.html.twig', array('candidats' => $listCandidat));
+    }
+    
+    public function ajoutCandidatAction() {
         $candidat = new Candidat();
         $form = $this->createForm(new CandidatType(), $candidat);
-        
+
         $request = $this->getRequest();
         $form->handleRequest($request);
-        
-        if($form->isValid()){
+
+        if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $em->persist($candidat);
             $em->flush();
+            return $this->redirect($this->generateUrl('upmc_suivi_stagiaire_accueil'));
         }
-        
-        return $this->render('UpmcSuiviStagiaireBundle::ajoutCandidat.html.twig',array('form'=> $form->createView()));
+
+        return $this->render('UpmcSuiviStagiaireBundle:candidat:ajoutCandidat.html.twig', array('form' => $form->createView()));
     }
+
+    public function modifCandidatAction($id) {
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository("\Upmc\SuiviStagiaireBundle\Entity\Candidat");
+
+        $candidat = $repository->find($id);
+        
+        $form = $this->createForm(new CandidatType(), $candidat);
+
+        $request = $this->getRequest();
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($candidat);
+            $em->flush();
+            return $this->redirect($this->generateUrl('upmc_suivi_stagiaire_accueil'));
+        }
+
+        return $this->render('UpmcSuiviStagiaireBundle:candidat:modifCandidat.html.twig', array('candidat' => $candidat));
+    }
+
 }
